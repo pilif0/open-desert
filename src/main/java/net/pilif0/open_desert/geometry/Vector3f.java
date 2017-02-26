2a019aa3f206148e137daf8f3ca3c3a5981ff2c4
Filip Smola <smola.filip@hotmail.com>
2017-02-26 13:29:02 +0000

    Added a representation of a vector in space

---
+++ src/main/java/net/pilif0/open_desert/geometry/Vector3f.java
@@ -0,0 +1,250 @@
+package net.pilif0.open_desert.geometry;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * Represents a vector in three dimensions.
+ *
+ * @author Filip Smola
+ * @version 1.0
+ */
+public class Vector3f {
+    //Standard vectors
+    /** The zero vector */
+    public static final Vector3f ZERO = new Vector3f(0, 0, 0);
+    /** The direction of the positive side of the x axis */
+    public static final Vector3f AXIS_X = new Vector3f(1, 0, 0);
+    /** The direction of the positive side of the y axis */
+    public static final Vector3f AXIS_Y = new Vector3f(0, 1, 0);
+    /** The direction of the positive side of the z axis */
+    public static final Vector3f AXIS_Z = new Vector3f(0, 0, 1);
+
+    /** The x coordinate */
+    public final float x;
+    /** The y coordinate */
+    public final float y;
+    /** The z coordinate */
+    public final float z;
+
+    /** The magnitude squared cache (to speed up repeated access) */
+    private float magnitudeSquared;
+
+    /**
+     * Constructs the vector from its coordinates
+     *
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param z The z coordinate
+     */
+    public Vector3f(float x, float y, float z){
+        //Make sure there are no -0
+        if(x == -0d) x = 0;
+        if(y == -0d) y = 0;
+        if(z == -0d) z = 0;
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Adds another vector to this one
+     *
+     * @param b The vector to add
+     * @return The resulting vector
+     */
+    public Vector3f add(@NotNull Vector3f b){
+        //Add components
+        return new Vector3f(
+                x + b.x,
+                y + b.y,
+                z + b.z);
+    }
+
+    /**
+     * Subtracts another vector from this one
+     *
+     * @param b The vector to subtract
+     * @return The resulting vector
+     */
+    public Vector3f subtract(@NotNull Vector3f b){
+        //Subtract components
+        return new Vector3f(
+                x - b.x,
+                y - b.y,
+                z - b.z
+        );
+    }
+
+    /**
+     * Scales this vector by a factor
+     *
+     * @param f The factor
+     * @return The resulting vector
+     */
+    public Vector3f scale(float f){
+        //Scale the components
+        return new Vector3f(
+                f * x,
+                f * y,
+                f * z
+        );
+    }
+
+    /**
+     * Calculates the cross product of this vector with another one
+     *
+     * @param b The other vector
+     * @return The cross product of the vectors
+     */
+    public Vector3f cross(@NotNull Vector3f b){
+        //Calculate the cross product this x b
+        return new Vector3f(
+                y * b.z - z * b.y,
+                z * b.x - x * b.z,
+                x * b.y - y * b.x);
+    }
+
+    /**
+     * Returns the unit vector with the same direction
+     *
+     * @return The unit vector with the same direction
+     */
+    public Vector3f normalize(){
+        //TODO decide what to do with zero vector - let it throw ArithmeticException or return zero vector?
+        return new Vector3f(
+                x / magnitude(),
+                y / magnitude(),
+                z / magnitude()
+        );
+    }
+
+    /**
+     * Returns the magnitude of the vector
+     *
+     * @return The magnitude of the vector
+     */
+    public float magnitude(){
+        return (float) Math.sqrt(magnitudeSquared());
+    }
+
+    /**
+     * Returns the magnitude of the vector squared
+     *
+     * @return The magnitude of the vector squared
+     */
+    public float magnitudeSquared(){
+        if(magnitudeSquared == 0 && !isZero()){
+            magnitudeSquared = x*x + y*y + z*z;
+        }
+
+        return magnitudeSquared;
+    }
+
+    /**
+     * Calculates the dot product of this vector and another one
+     *
+     * @param b The other vector
+     * @return The dot product
+     */
+    public float dot(@NotNull Vector3f b){
+        return x * b.x + y * b.y + z * b.z;
+    }
+
+    /**
+     * Returns whether this vector is a multiple of another one
+     *
+     * @param b The vector to test against
+     * @return Whether this vector is a multiple of the other one
+     */
+    public boolean isMultipleOf(@NotNull Vector3f b){
+        return (dot(b) * dot(b)) / b.magnitudeSquared() == magnitudeSquared();
+    }
+
+    /**
+     * Returns the projection of this vector onto another one
+     *
+     * @param b The vector to project onto
+     * @return The projection
+     */
+    public Vector3f projectOnto(@NotNull Vector3f b){
+        float a = dot(b) / b.magnitudeSquared();
+        return b.scale(a);
+    }
+
+    /**
+     * Returns the projection of this vector onto a pair of other vectors (a plane)
+     *
+     * @param u The first vector to project onto
+     * @param v The second vector to project onto
+     * @return The projection
+     */
+    public Vector3f projectOnto(@NotNull Vector3f u, @NotNull Vector3f v){
+        return subtract(projectOnto(u.cross(v)));
+    }
+
+    /**
+     * Returns the perpendicular component of this vector to another one
+     *
+     * @param b The other vector
+     * @return The perpendicular component
+     */
+    public Vector3f perpComponent(@NotNull Vector3f b){
+        return subtract(projectOnto(b));
+    }
+
+    /**
+     * Returns the perpendicular component of this vector to other two (a plane)
+     *
+     * @param u The first vector
+     * @param v The second vector
+     * @return The perpendicular component
+     */
+    public Vector3f perpComponent(@NotNull Vector3f u, @NotNull Vector3f v){
+        return projectOnto(u.cross(v));
+    }
+
+    /**
+     * Returns the angle between this vector and another
+     *
+     * @param b The other vector
+     * @return The angle
+     */
+    public double angle(@NotNull Vector3f b){
+        return Math.acos(dot(b) / (magnitude() * b.magnitude()));
+    }
+
+    /**
+     * Returns whether this vector is the zero vector
+     *
+     * @return Whether this vector is the zero vector
+     */
+    public boolean isZero(){
+        return x == 0 && y == 0 && z == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vector3f vector3f = (Vector3f) o;
+
+        if (Float.compare(vector3f.x, x) != 0) return false;
+        if (Float.compare(vector3f.y, y) != 0) return false;
+        return Float.compare(vector3f.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (x != +0.0f ? Float.floatToIntBits(x) : 0);
+        result = 31 * result + (y != +0.0f ? Float.floatToIntBits(y) : 0);
+        result = 31 * result + (z != +0.0f ? Float.floatToIntBits(z) : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%f, %f, %f]", x, y, z);
+    }
+}
---
+++ src/test/java/net/pilif0/open_desert/geometry/Vector3fTest.java
@@ -0,0 +1,245 @@
+package net.pilif0.open_desert.geometry;
+
+import net.pilif0.open_desert.geometry.Vector3f;
+import org.junit.Test;
+
+import java.util.stream.DoubleStream;
+
+import static net.pilif0.open_desert.geometry.Vector3f.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * A set of unit tests for the {@code Vector3f} class
+ *
+ * @author Filip Smola
+ * @version 1.0
+ */
+public class Vector3fTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        //Tests vector addition
+        assertEquals(
+                new Vector3f(1, 1, 1),
+                (new Vector3f(1, 0, 1)).add(new Vector3f(0, 1, 0))
+        );
+        assertEquals(
+                new Vector3f(1, 2, 3),
+                (new Vector3f(1, 1, 1))
+                        .add(new Vector3f(0, 1, 1))
+                        .add(new Vector3f(0, 0, 1))
+        );
+        assertEquals(
+                new Vector3f(0, 0, 0),
+                (new Vector3f(-2, 3, -5)).add(new Vector3f(2, -3, 5))
+        );
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        //Tests vector subtraction
+        assertEquals(
+                new Vector3f(1, -1, 1),
+                (new Vector3f(1, 0, 1)).subtract(new Vector3f(0, 1, 0))
+        );
+        assertEquals(
+                new Vector3f(1, 0, -1),
+                (new Vector3f(1, 1, 1))
+                        .subtract(new Vector3f(0, 1, 1))
+                        .subtract(new Vector3f(0, 0, 1))
+        );
+        assertEquals(
+                new Vector3f(-4, 6, -10),
+                (new Vector3f(-2, 3, -5)).subtract(new Vector3f(2, -3, 5))
+        );
+    }
+
+    @Test
+    public void testScale() throws Exception {
+        //Test vector scaling
+        assertEquals(
+                new Vector3f(2, 4, 6),
+                (new Vector3f(1, 2, 3)).scale(2)
+        );
+        assertEquals(
+                new Vector3f(0, 0, 0),
+                (new Vector3f(1, 2, 3)).scale(0)
+        );
+        assertEquals(
+                new Vector3f(-1, -2, -3),
+                (new Vector3f(1, 2, 3)).scale(-1)
+        );
+    }
+
+    @Test
+    public void testCross() throws Exception {
+        //Test cross product of vectors
+        assertEquals(
+                AXIS_Z,
+                AXIS_X.cross(AXIS_Y)
+        );
+        assertEquals(
+                ZERO,
+                AXIS_X.cross(AXIS_X)
+        );
+        assertEquals(
+                new Vector3f(0, 0, -8),
+                (new Vector3f(2, 2, 0)).cross(new Vector3f(2, -2, 0))
+        );
+    }
+
+    @Test
+    public void testNormalize() throws Exception {
+        //Test vector normalisation
+        assertEquals(
+                AXIS_Z,
+                (new Vector3f(0, 0, 13)).normalize()
+        );
+        assertEquals(
+                new Vector3f(0, -1, 0),
+                (new Vector3f(0, -3, 0)).normalize()
+        );
+    }
+
+    @Test
+    public void testMagnitude() throws Exception {
+        //Test vector magnitude
+        assertEquals(
+                1,
+                AXIS_X.magnitude(),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                0,
+                ZERO.magnitude(),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                5,
+                (new Vector3f(3, 4, 0)).magnitude(),
+                Float.MIN_NORMAL
+        );
+    }
+
+    @Test
+    public void testDot() throws Exception {
+        //Test vector dot product
+        assertEquals(
+                0,
+                AXIS_X.dot(AXIS_Y),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                12,
+                (new Vector3f(1, 2, 3)).dot(new Vector3f(6, 3, 0)),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                -1,
+                (new Vector3f(1, 2, 3)).dot(new Vector3f(0, 1, -1)),
+                Float.MIN_NORMAL
+        );
+    }
+
+    @Test
+    public void testIsMultipleOf() throws Exception {
+        //Test vector multiple verification
+        assertTrue((new Vector3f(3, 0, 0)).isMultipleOf(AXIS_X));
+        assertTrue((new Vector3f(3, 3, 0)).isMultipleOf(new Vector3f(1, 1, 0)));
+        assertTrue((new Vector3f(3, 0, 3)).isMultipleOf(new Vector3f(5, 0, 5)));
+
+        assertFalse((new Vector3f(3, 0 ,0)).isMultipleOf(AXIS_Y));
+        assertFalse((new Vector3f(3, 3 ,0)).isMultipleOf(AXIS_Y));
+        assertFalse((new Vector3f(3, 0 ,3)).isMultipleOf(AXIS_Y));
+    }
+
+    @Test
+    public void testProjectOnto1D() throws Exception {
+        //Test vector projection onto one vector
+        assertEquals(
+                new Vector3f(2, 0, 0),
+                (new Vector3f(2, 2, 15))
+                        .projectOnto(AXIS_X)
+        );
+        assertEquals(
+                new Vector3f(0, 0, 0),
+                (new Vector3f(-2, 2, 0))
+                        .projectOnto(new Vector3f(2, 2, 0))
+        );
+        assertEquals(
+                new Vector3f(2, 0, 0),
+                (new Vector3f(2, 2, 15))
+                        .projectOnto(AXIS_X)
+        );
+    }
+
+    @Test
+    public void testProjectOnto2D() throws Exception {
+        //Test vector projection onto two vectors
+        assertEquals(
+                new Vector3f(2, 2, 0),
+                (new Vector3f(2, 2, 42))
+                    .projectOnto(AXIS_X, AXIS_Y)
+        );
+        assertEquals(
+                new Vector3f(2, 0, 42),
+                (new Vector3f(2, 2, 42))
+                    .projectOnto(AXIS_X, AXIS_Z)
+        );
+        assertEquals(
+                new Vector3f(0, 2, 42),
+                (new Vector3f(2, 2, 42))
+                    .projectOnto(AXIS_Y, AXIS_Z)
+        );
+    }
+
+    @Test
+    public void testPerpComponent1D() throws Exception {
+        //Test perpendicular component calculation with respect to one vector
+        assertEquals(
+                new Vector3f(0, 0, 42),
+                (new Vector3f(2, 0,42))
+                    .perpComponent(AXIS_X)
+        );
+        assertEquals(
+                new Vector3f(2, 0, 0),
+                (new Vector3f(2, 0,42))
+                    .perpComponent(AXIS_Z)
+        );
+        assertEquals(
+                new Vector3f(1, 1, 0),
+                (new Vector3f(1, 1,42))
+                    .perpComponent(AXIS_Z)
+        );
+    }
+
+    @Test
+    public void testPerpComponent2D() throws Exception {
+        //Test perpendicular component calculation with respect to one vector
+        assertEquals(
+                new Vector3f(0, 0, 42),
+                (new Vector3f(2, 2,42))
+                        .perpComponent(AXIS_X, AXIS_Y)
+        );
+        assertEquals(
+                new Vector3f(2, 0, 0),
+                (new Vector3f(2, 13,42))
+                        .perpComponent(AXIS_Y, AXIS_Z)
+        );
+        assertEquals(
+                new Vector3f(0, 1, 0),
+                (new Vector3f(31, 1,42))
+                        .perpComponent(AXIS_X, AXIS_Z)
+        );
+    }
+
+    @Test
+    public void testAngle() throws Exception {
+        //Test calculation of the angle between two vectors
+        assertEquals(Math.toRadians(90), AXIS_X.angle(AXIS_Y), 10e-5);
+        assertEquals(Math.toRadians(0), AXIS_X.angle(AXIS_X), 10e-5);
+        assertEquals(Math.toRadians(45), AXIS_X.angle(new Vector3f(1, 1, 0)), 10-5);
+    }
+}
