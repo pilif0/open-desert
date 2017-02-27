97891c7f3a24bbf3da4722ce1011e81bd8ea620d
Filip Smola <smola.filip@hotmail.com>
2017-02-27 14:24:26 +0000

    Added a representation of a vector in a plane

---
+++ src/main/java/net/pilif0/open_desert/geometry/Vector2f.java
@@ -0,0 +1,202 @@
+package net.pilif0.open_desert.geometry;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * Represents a vector in two dimensions
+ *
+ * @author Filip Smola
+ * @version 1.0
+ */
+public class Vector2f {
+    //Standard vectors
+    /** The zero vector */
+    public static final Vector2f ZERO = new Vector2f(0, 0);
+    /** The direction of the positive side of the x axis */
+    public static final Vector2f AXIS_X = new Vector2f(1, 0);
+    /** The direction of the positive side of the y axis */
+    public static final Vector2f AXIS_Y = new Vector2f(0, 1);
+
+    /** The x coordinate */
+    public final float x;
+    /** The y coordinate */
+    public final float y;
+
+    /** The magnitude squared cache (to speed up repeated access) */
+    private float magnitudeSquared;
+
+    /**
+     * Constructs the vector from its coordinates
+     *
+     * @param x The x coordinate
+     * @param y The y coordinate
+     */
+    public Vector2f(float x, float y){
+        //Make sure there are no -0
+        if(x == -0d) x = 0;
+        if(y == -0d) y = 0;
+
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Adds another vector to this one
+     *
+     * @param b The vector to add
+     * @return The resulting vector
+     */
+    public Vector2f add(@NotNull Vector2f b){
+        //Add components
+        return new Vector2f(
+                x + b.x,
+                y + b.y
+        );
+    }
+
+    /**
+     * Subtracts another vector from this one
+     *
+     * @param b The vector to subtract
+     * @return The resulting vector
+     */
+    public Vector2f subtract(@NotNull Vector2f b){
+        //Subtract components
+        return new Vector2f(
+                x - b.x,
+                y - b.y
+        );
+    }
+
+    /**
+     * Scales this vector by a factor
+     *
+     * @param f The factor
+     * @return The resulting vector
+     */
+    public Vector2f scale(float f){
+        //Scale the components
+        return new Vector2f(
+                f * x,
+                f * y
+        );
+    }
+
+    /**
+     * Returns the unit vector with the same direction
+     *
+     * @return The unit vector with the same direction
+     */
+    public Vector2f normalize(){
+        //TODO decide what to do with zero vector - let it throw ArithmeticException or return zero vector?
+        return new Vector2f(
+                x / magnitude(),
+                y / magnitude()
+        );
+    }
+
+    /**
+     * Returns the magnitude of the vector
+     *
+     * @return The magnitude of the vector
+     */
+    public float magnitude(){
+        return (float) Math.sqrt(magnitudeSquared());
+    }
+
+    /**
+     * Returns the magnitude of the vector squared
+     *
+     * @return The magnitude of the vector squared
+     */
+    public float magnitudeSquared(){
+        if(magnitudeSquared == 0 && !isZero()){
+            magnitudeSquared = x*x + y*y;
+        }
+
+        return magnitudeSquared;
+    }
+
+    /**
+     * Calculates the dot product of this vector and another one
+     *
+     * @param b The other vector
+     * @return The dot product
+     */
+    public float dot(@NotNull Vector2f b){
+        return x * b.x + y * b.y;
+    }
+
+    /**
+     * Returns whether this vector is a multiple of another one
+     *
+     * @param b The vector to test against
+     * @return Whether this vector is a multiple of the other one
+     */
+    public boolean isMultipleOf(@NotNull Vector2f b){
+        return (dot(b) * dot(b)) / b.magnitudeSquared() == magnitudeSquared();
+    }
+
+    /**
+     * Returns the projection of this vector onto another one
+     *
+     * @param b The vector to project onto
+     * @return The projection
+     */
+    public Vector2f projectOnto(@NotNull Vector2f b){
+        float a = dot(b) / b.magnitudeSquared();
+        return b.scale(a);
+    }
+
+    /**
+     * Returns the perpendicular component of this vector to another one
+     *
+     * @param b The other vector
+     * @return The perpendicular component
+     */
+    public Vector2f perpComponent(@NotNull Vector2f b){
+        return subtract(projectOnto(b));
+    }
+
+    /**
+     * Returns the angle between this vector and another
+     *
+     * @param b The other vector
+     * @return The angle
+     */
+    public double angle(@NotNull Vector2f b){
+        return Math.acos(dot(b) / (magnitude() * b.magnitude()));
+    }
+
+    /**
+     * Returns whether this vector is the zero vector
+     *
+     * @return Whether this vector is the zero vector
+     */
+    public boolean isZero(){
+        return x == 0 && y == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vector2f vector2f = (Vector2f) o;
+
+        if (Float.compare(vector2f.x, x) != 0) return false;
+        return Float.compare(vector2f.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (x != +0.0f ? Float.floatToIntBits(x) : 0);
+        result = 31 * result + (y != +0.0f ? Float.floatToIntBits(y) : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%f, %f]", x, y);
+    }
+}
---
+++ src/test/java/net/pilif0/open_desert/geometry/Vector2fTest.java
@@ -0,0 +1,180 @@
+package net.pilif0.open_desert.geometry;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static net.pilif0.open_desert.geometry.Vector2f.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * A set of unit tests for the {@code Vector2f} class
+ *
+ * @author Filip Smola
+ * @version 1.0
+ */
+public class Vector2fTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        //Tests vector addition
+        assertEquals(
+                new Vector2f(1, 1),
+                (new Vector2f(1, 0)).add(new Vector2f(0, 1))
+        );
+        assertEquals(
+                new Vector2f(1, 3),
+                (new Vector2f(1, 1))
+                        .add(new Vector2f(0, 1))
+                        .add(new Vector2f(0, 1))
+        );
+        assertEquals(
+                new Vector2f(0, 0),
+                (new Vector2f(-2, 3)).add(new Vector2f(2, -3))
+        );
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        //Tests vector subtraction
+        assertEquals(
+                new Vector2f(1, -1),
+                (new Vector2f(1, 0)).subtract(new Vector2f(0, 1))
+        );
+        assertEquals(
+                new Vector2f(0, 0),
+                (new Vector2f(1, 1))
+                        .subtract(new Vector2f(0, 1))
+                        .subtract(new Vector2f(1, 0))
+        );
+        assertEquals(
+                new Vector2f(-4, 6),
+                (new Vector2f(-2, 3)).subtract(new Vector2f(2, -3))
+        );
+    }
+
+    @Test
+    public void testScale() throws Exception {
+        //Test vector scaling
+        assertEquals(
+                new Vector2f(2, 4),
+                (new Vector2f(1, 2)).scale(2)
+        );
+        assertEquals(
+                new Vector2f(0, 0),
+                (new Vector2f(1, 2)).scale(0)
+        );
+        assertEquals(
+                new Vector2f(-1, -2),
+                (new Vector2f(1, 2)).scale(-1)
+        );
+    }
+
+    @Test
+    public void testNormalize() throws Exception {
+        //Test vector normalisation
+        assertEquals(
+                AXIS_Y,
+                (new Vector2f(0, 15)).normalize()
+        );
+        assertEquals(
+                new Vector2f(0, -1),
+                (new Vector2f(0, -3)).normalize()
+        );
+    }
+
+    @Test
+    public void testMagnitude() throws Exception {
+        //Test vector magnitude
+        assertEquals(
+                1,
+                AXIS_X.magnitude(),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                0,
+                ZERO.magnitude(),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                5,
+                (new Vector2f(3, 4)).magnitude(),
+                Float.MIN_NORMAL
+        );
+    }
+
+    @Test
+    public void testDot() throws Exception {
+        //Test vector dot product
+        assertEquals(
+                0,
+                AXIS_X.dot(AXIS_Y),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                12,
+                (new Vector2f(1, 2)).dot(new Vector2f(6, 3)),
+                Float.MIN_NORMAL
+        );
+        assertEquals(
+                2,
+                (new Vector2f(1, 2)).dot(new Vector2f(0, 1)),
+                Float.MIN_NORMAL
+        );
+    }
+
+    @Test
+    public void testIsMultipleOf() throws Exception {
+        //Test vector multiple verification
+        assertTrue((new Vector2f(3, 0)).isMultipleOf(AXIS_X));
+        assertTrue((new Vector2f(3, 3)).isMultipleOf(new Vector2f(1, 1)));
+        assertTrue((new Vector2f(3, 0)).isMultipleOf(new Vector2f(5, 0)));
+
+        assertFalse((new Vector2f(3, 0)).isMultipleOf(AXIS_Y));
+        assertFalse((new Vector2f(3, 3)).isMultipleOf(AXIS_Y));
+        assertFalse((new Vector2f(3, 0)).isMultipleOf(AXIS_Y));
+    }
+
+    @Test
+    public void testProjectOnto() throws Exception {
+        //Test vector projection onto one vector
+        assertEquals(
+                new Vector2f(2, 0),
+                (new Vector2f(2, 2))
+                        .projectOnto(AXIS_X)
+        );
+        assertEquals(
+                new Vector2f(0, 0),
+                (new Vector2f(-2, 2))
+                        .projectOnto(new Vector2f(2, 2))
+        );
+        assertEquals(
+                new Vector2f(2, 0),
+                (new Vector2f(2, 2))
+                        .projectOnto(AXIS_X)
+        );
+    }
+
+    @Test
+    public void testPerpComponent() throws Exception {
+        //Test perpendicular component calculation with respect to one vector
+        assertEquals(
+                new Vector2f(0, 0),
+                (new Vector2f(2, 0))
+                        .perpComponent(AXIS_X)
+        );
+        assertEquals(
+                new Vector2f(2, 0),
+                (new Vector2f(2, 0))
+                        .perpComponent(AXIS_Y)
+        );
+    }
+
+    @Test
+    public void testAngle() throws Exception {
+        //Test calculation of the angle between two vectors
+        assertEquals(Math.toRadians(90), AXIS_X.angle(AXIS_Y), 10e-5);
+        assertEquals(Math.toRadians(0), AXIS_X.angle(AXIS_X), 10e-5);
+        assertEquals(Math.toRadians(45), AXIS_X.angle(new Vector2f(1, 1)), 10e-5);
+    }
+}
