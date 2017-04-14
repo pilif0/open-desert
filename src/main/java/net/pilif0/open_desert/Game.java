af0d0d7085d22d448af55b2b7500019a6ee9f520
Filip Smola <smola.filip@hotmail.com>
2017-04-14 18:19:09 +0100

    Made sure that the game cleans up the static shaders

--- src/main/java/net/pilif0/open_desert/Game.java
+++ src/main/java/net/pilif0/open_desert/Game.java
@@ -1,6 +1,7 @@
 package net.pilif0.open_desert;
 
 import javafx.util.Pair;
+import net.pilif0.open_desert.graphics.ShaderProgram;
 import net.pilif0.open_desert.input.Action;
 import net.pilif0.open_desert.state.GameState;
 import net.pilif0.open_desert.state.StateMachine;
@@ -156,6 +157,10 @@ public class Game {
      * Cleans up
      */
     private void cleanUp(){
+        //Clean up all the global shaders
+        ShaderProgram.COLORED_SHADER.cleanUp();
+        ShaderProgram.BASIC_SHADER.cleanUp();
+
         states.cleanUp();
         window.destroy();
         glfwTerminate();
