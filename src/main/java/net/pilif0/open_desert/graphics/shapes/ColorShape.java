ebb99b537330bf8b6942864b423755d3c7e57c69
Filip Smola <smola.filip@hotmail.com>
2017-06-05 12:01:02 +0200

    Added shape file formats and parsers to the basic and colored shapes

--- src/main/java/net/pilif0/open_desert/graphics/shapes/ColorShape.java
+++ src/main/java/net/pilif0/open_desert/graphics/shapes/ColorShape.java
@@ -3,9 +3,15 @@ package net.pilif0.open_desert.graphics.shapes;
 import net.pilif0.open_desert.Launcher;
 import net.pilif0.open_desert.graphics.vertices.ColorVertex;
 import org.lwjgl.system.MemoryStack;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.*;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.nio.file.Path;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
@@ -20,6 +26,39 @@ import static org.lwjgl.opengl.GL30.*;
  * @version 1.0
  */
 public class ColorShape extends AbstractShape{
+    //Parser XPath expressions
+    private static final XPathExpression typeExpr;
+    private static final XPathExpression idExpr;
+    private static final XPathExpression posXExpr;
+    private static final XPathExpression posYExpr;
+    private static final XPathExpression colRExpr;
+    private static final XPathExpression colGExpr;
+    private static final XPathExpression colBExpr;
+    private static final XPathExpression colAExpr;
+    private static final XPathExpression indexExpr;
+
+    static{
+        //Prepare to use XPath
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+
+        //Compile the XPath expressions
+        try {
+            typeExpr = xpath.compile("/shape/@type");
+            idExpr = xpath.compile("//vertex/@id");
+            posXExpr = xpath.compile("//vertex/position/x");
+            posYExpr = xpath.compile("//vertex/position/y");
+            colRExpr = xpath.compile("//vertex/color/r");
+            colGExpr = xpath.compile("//vertex/color/g");
+            colBExpr = xpath.compile("//vertex/color/b");
+            colAExpr = xpath.compile("//vertex/color/a");
+            indexExpr = xpath.compile("/shape/indices/index/text()");
+        }catch(XPathExpressionException e){
+            //When XPath compilation fails, there is no hope. Just die.
+            throw new RuntimeException("ColorShape XPath compilation failed");
+        }
+    }
+
     /**
      * Constructs the shape
      *
@@ -87,6 +126,83 @@ public class ColorShape extends AbstractShape{
         // Delete the VAO
         glBindVertexArray(0);
         glDeleteVertexArrays(vaoID);
+    }
+
+    /**
+     * Parses a shape file of ColorShape type into a ColorShape object
+     *
+     * @param p The path to the shape file
+     * @return The parsed object
+     * @throws ShapeParseException When object is in wrong format
+     */
+    public static ColorShape parse(Path p){
+        //Parse the shape file (XML)
+        Document doc;
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            doc = builder.parse(p.toFile());
+        }catch(Exception e){
+            throw new ShapeParseException(p, e);
+        }
+
+        //Verify the type
+        String type;
+        try {
+            type = (String) typeExpr.evaluate(doc, XPathConstants.STRING);
+        } catch (XPathExpressionException e) {
+            //Assume wrong type when evaluation fails
+            type = "wrong";
+        }
+        if(!type.equals("ColorShape")){
+            throw new ShapeParseException(p, "Wrong type");
+        }
+
+        //Gather all vertex nodes
+        ColorVertex[] vertices;
+        try{
+            NodeList idNodes = (NodeList) idExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList xNodes = (NodeList) posXExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList yNodes = (NodeList) posYExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList colRNodes = (NodeList) colRExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList colGNodes = (NodeList) colGExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList colBNodes = (NodeList) colBExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList colANodes = (NodeList) colAExpr.evaluate(doc, XPathConstants.NODESET);
+
+            vertices = new ColorVertex[idNodes.getLength()];
+
+            for(int i = 0; i < idNodes.getLength(); i++){
+                int id = Integer.parseInt(idNodes.item(i).getTextContent());
+                float x = Float.parseFloat(xNodes.item(i).getTextContent());
+                float y = Float.parseFloat(yNodes.item(i).getTextContent());
+                float r = Float.parseFloat(colRNodes.item(i).getTextContent());
+                float g = Float.parseFloat(colGNodes.item(i).getTextContent());
+                float b = Float.parseFloat(colBNodes.item(i).getTextContent());
+                float a = Float.parseFloat(colANodes.item(i).getTextContent());
+
+                ColorVertex vertex = new ColorVertex(x, y, r, g, b, a);
+                vertices[id] = vertex;
+            }
+        }catch(XPathExpressionException e){
+            throw new ShapeParseException(p, e);
+        }
+
+        //Gather the indices
+        int[] indices;
+        try{
+            NodeList indexNodes = (NodeList) indexExpr.evaluate(doc, XPathConstants.NODESET);
+
+            indices = new int[indexNodes.getLength()];
+
+            for(int i = 0; i < indexNodes.getLength(); i++){
+                indices[i] = Integer.parseInt(indexNodes.item(i).getTextContent());
+            }
+        }catch(XPathExpressionException e){
+            throw new ShapeParseException(p, e);
+        }
+
+        //Put the shape together and return it
+        return new ColorShape(vertices, indices);
     }
 
 }
--- src/main/java/net/pilif0/open_desert/graphics/shapes/Shape.java
+++ src/main/java/net/pilif0/open_desert/graphics/shapes/Shape.java
@@ -3,9 +3,15 @@ package net.pilif0.open_desert.graphics.shapes;
 import net.pilif0.open_desert.Launcher;
 import net.pilif0.open_desert.graphics.vertices.Vertex;
 import org.lwjgl.system.MemoryStack;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.*;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.nio.file.Path;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
@@ -25,6 +31,31 @@ import static org.lwjgl.opengl.GL30.glGenVertexArrays;
  * @version 1.0
  */
 public class Shape extends AbstractShape{
+    //Parser XPath expressions
+    private static final XPathExpression typeExpr;
+    private static final XPathExpression idExpr;
+    private static final XPathExpression posXExpr;
+    private static final XPathExpression posYExpr;
+    private static final XPathExpression indexExpr;
+
+    static{
+        //Prepare to use XPath
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+
+        //Compile the XPath expressions
+        try {
+            typeExpr = xpath.compile("/shape/@type");
+            idExpr = xpath.compile("//vertex/@id");
+            posXExpr = xpath.compile("//vertex/position/x");
+            posYExpr = xpath.compile("//vertex/position/y");
+            indexExpr = xpath.compile("/shape/indices/index/text()");
+        }catch(XPathExpressionException e){
+            //When XPath compilation fails, there is no hope. Just die.
+            throw new RuntimeException("Shape XPath compilation failed");
+        }
+    }
+
     /**
      * Constructs the shape
      *
@@ -90,6 +121,75 @@ public class Shape extends AbstractShape{
         // Delete the VAO
         glBindVertexArray(0);
         glDeleteVertexArrays(vaoID);
+    }
+
+    /**
+     * Parses a shape file of Shape type into a Shape object
+     *
+     * @param p The path to the shape file
+     * @return The parsed object
+     * @throws ShapeParseException When object is in wrong format
+     */
+    public static Shape parse(Path p){
+        //Parse the shape file (XML)
+        Document doc;
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            doc = builder.parse(p.toFile());
+        }catch(Exception e){
+            throw new ShapeParseException(p, e);
+        }
+
+        //Verify the type
+        String type;
+        try {
+            type = (String) typeExpr.evaluate(doc, XPathConstants.STRING);
+        } catch (XPathExpressionException e) {
+            //Assume wrong type when evaluation fails
+            type = "wrong";
+        }
+        if(!type.equals("Shape")){
+            throw new ShapeParseException(p, "Wrong type");
+        }
+
+        //Gather all vertex nodes
+        Vertex[] vertices;
+        try{
+            NodeList idNodes = (NodeList) idExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList xNodes = (NodeList) posXExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList yNodes = (NodeList) posYExpr.evaluate(doc, XPathConstants.NODESET);
+
+            vertices = new Vertex[idNodes.getLength()];
+
+            for(int i = 0; i < idNodes.getLength(); i++){
+                int id = Integer.parseInt(idNodes.item(i).getTextContent());
+                float x = Float.parseFloat(xNodes.item(i).getTextContent());
+                float y = Float.parseFloat(yNodes.item(i).getTextContent());
+
+                Vertex vertex = new Vertex(x, y);
+                vertices[id] = vertex;
+            }
+        }catch(XPathExpressionException e){
+            throw new ShapeParseException(p, e);
+        }
+
+        //Gather the indices
+        int[] indices;
+        try{
+            NodeList indexNodes = (NodeList) indexExpr.evaluate(doc, XPathConstants.NODESET);
+
+            indices = new int[indexNodes.getLength()];
+
+            for(int i = 0; i < indexNodes.getLength(); i++){
+                indices[i] = Integer.parseInt(indexNodes.item(i).getTextContent());
+            }
+        }catch(XPathExpressionException e){
+            throw new ShapeParseException(p, e);
+        }
+
+        //Put the shape together and return it
+        return new Shape(vertices, indices);
     }
 
 }
